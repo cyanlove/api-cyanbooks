6a62403a9588bff0907c74a1162f3eedecf1bd6d
Jaco Baldrich <jb@jacobaldrich.com>
2020-02-11 16:25:23 +0100

    Fix tests

--- src/Shared/Domain/StringValueObject.php
+++ src/Shared/Domain/StringValueObject.php
@@ -4,32 +4,32 @@ namespace CyanBooks\Shared\Domain;
 
 abstract class StringValueObject
 {
-	/* @var string */
-	protected $value;
+    /* @var string */
+    protected $value;
 
-	public function __construct(string $value)
-	{
-		$this->validate($value);
-		$this->value = $value;
-	}
-
-	public function value(): string
-	{
-		return $this->value;
-	}
-
-	public function __toString(): string
+    public function __construct(string $value)
     {
-		return $this->value();
-	}
+        $this->validate($value);
+        $this->value = $value;
+    }
 
-	/**
-	 * Guard clause to check if the value follows the
-	 * required parameters.
-	 *
-	 * @param string $value
-	 * @throws \InvalidArgumentException
-	 * @return void
-	 */
-	abstract protected function validate(string $value);
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->value();
+    }
+
+    /**
+     * Guard clause to check if the value follows the
+     * required parameters.
+     *
+     * @param string $value
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    abstract protected function validate(string $value);
 }
