39877915b56f69d8e68d7fef48c4d4caf354857b
Jaco Baldrich <jb@jacobaldrich.com>
2020-02-24 17:27:28 +0100

    Add Mockery

--- tests/Shared/TestCase.php
+++ tests/Shared/TestCase.php
@@ -2,8 +2,22 @@
 
 namespace CyanBooks\Test\Book\Shared;
 
+use Mockery;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 abstract class TestCase extends PHPUnitTestCase
 {
+    use MockeryPHPUnitIntegration;
+
+    public function mock(string $class): MockInterface
+    {
+        return Mockery::mock($class);
+    }
+
+    public function similarTo($arguments)
+    {
+        return \Hamcrest\Matchers::equalTo($arguments);
+    }
 }
